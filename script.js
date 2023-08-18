d5837f8a7a4c2d668c8a0ef87a9d88e5d9dc1801
rihhana <hrihhana@gmail.com>
2023-08-18 19:43:33 +0300

    answrs of find-7

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Good Luck 💪🏾
+function findSeven(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].toString().includes('7')) {
+        return 'Found!';
+      }
+    }
+    return 'there is no 7 in the array';
+  }
+  
